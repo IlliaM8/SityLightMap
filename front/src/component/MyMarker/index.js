b81529aea85edde724031f1048cbec21a896c163
Illia <strelchuk.ilua@gmail.com>
2022-12-09 23:09:34 +0200

    d

---
+++ front/src/component/MyMarker/index.js
@@ -0,0 +1,8 @@
+export default class Marker {
+  constructor(coords, description, time, id) {
+    this.coords = coords;
+    this.description = description;
+    this.time = time;
+    this.id = id;
+  }
+}
